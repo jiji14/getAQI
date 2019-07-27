297af6e87e55d098597aff247fdf419b75c28b11
jiji <jijeong0114@gmail.com>
2019-07-27 18:11:20 +0900

    Updates

---
+++ precache-manifest.62ebd60a77c7ecf245ead875e77d4d95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f876d2668ccc16c7314be885de67c471",
+    "url": "/getAQI/index.html"
+  },
+  {
+    "revision": "c72e6f9ff907b974a216",
+    "url": "/getAQI/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "95cd2833a52a0ed71a79",
+    "url": "/getAQI/static/css/main.b40afef3.chunk.css"
+  },
+  {
+    "revision": "c72e6f9ff907b974a216",
+    "url": "/getAQI/static/js/2.771d23de.chunk.js"
+  },
+  {
+    "revision": "95cd2833a52a0ed71a79",
+    "url": "/getAQI/static/js/main.125bd8df.chunk.js"
+  },
+  {
+    "revision": "31c20a605c26985e7e50",
+    "url": "/getAQI/static/js/runtime~main.e1113ab8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.58e5687a061991fb456ac1e6b7e9f227.js"
+  "/getAQI/precache-manifest.62ebd60a77c7ecf245ead875e77d4d95.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/getAQI/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
